f1fff8cd95e43a0436f444e3fb700bae0748f9db
Sweyd Manaf <abdulsweyd@gmail.com>
2022-11-26 00:02:57 +0200

    Update DBAdmin.py

--- DBAdmin.py
+++ DBAdmin.py
@@ -4,8 +4,7 @@ import sqlite3
 
 class BancoDeDados:
     def __init__(self):
-        # self.conexao.row_factory = sqlite3.Row
-
+        self.conexao = sqlite3.connect('dataBase.db')
         self.stateDB = os.path.isfile('dataBase.db')
 
         if not self.stateDB:
@@ -21,16 +20,17 @@ class BancoDeDados:
                                 )
 
     def insert_new_part(self, partName, customer, retailer, price):
-        with sqlite3.connect('dataBase.db') as conexao:
+        with self.conexao as conexao:
             if self.verify_if_part_exists(partName, customer, retailer, price):
                 conexao.execute("INSERT INTO pecas(partName, customer, retailer, price) VALUES(?, ?, ?, ?)",
                                 (partName, customer, retailer, price))
+                return True
             else:
                 return False
 
     def verify_if_part_exists(self, partName, customer, retailer, price):
         repeat = 1
-        with sqlite3.connect('dataBase.db') as conexao:
+        with self.conexao as conexao:
             for registro in conexao.execute('SELECT * FROM pecas'):
                 if partName == registro[1] and customer == registro[2] and retailer == registro[3] and price == str(
                         registro[4]):
@@ -41,11 +41,15 @@ class BancoDeDados:
         return repeat
 
     def list_parts(self):
-        with sqlite3.connect('dataBase.db') as conexao:
+        with self.conexao as conexao:
             for registro in conexao.execute('SELECT * FROM pecas'):
                 yield registro
 
     def delete_part(self, id):
-        with sqlite3.connect('dataBase.db') as conexao:
+        with self.conexao as conexao:
             conexao.execute('DELETE FROM pecas WHERE id=?', id)
-            conexao.commit()
+
+    def update_part(self, id, partname, customer, retailer, price):
+        with self.conexao as conexao:
+            conexao.execute('UPDATE pecas SET partName=?, customer=?, retailer=?, price=? WHERE id=?',
+                            (partname, customer, retailer, price, id))
